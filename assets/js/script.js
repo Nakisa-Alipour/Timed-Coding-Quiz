554c5737e41d159ef3957d1f40a8e75bbc3dda8c
n.alipour777@yahoo.com <n.alipour777@yahoo.com>
2023-03-11 17:20:13 +1100

    startGame and tiner functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,19 @@
 var extraInfo= document.querySelector(".extra-info");
 var startButton= document.querySelector(".start-button");
-var highscoreButton= document.querySelector("highscore-button");
-var countdown = document.querySelector(".countdown");
+var highscoreButton= document.querySelector(".highscore-button");
+var countdown = document.querySelector(".count-down");
 var questionArea = document.querySelector(".question-area");
-var multiChoiceArea = document.querySelector(".multichoice-area");
+var multipleChoiceArea = document.querySelector(".multiplechoice-area");
 var checkAnswer = document.querySelector(".check-answer");
 var correctAnswer = document.querySelector(".correct-answer");
-var correctAnswer = document.querySelector(".wrong-answer");
+var wrongAnswer = document.querySelector(".wrong-answer");
+var resultSubmission = document.querySelector(".result-submission");
+
+var intialInput = document.getElementById("initial");
+var submissionButton = document.getElementById("submit");
 
 
+var timeleft;
 
 
 
@@ -16,34 +21,45 @@ var correctAnswer = document.querySelector(".wrong-answer");
 
 
 function startGame() {
-  isWin = false;
   timerCount = 60;
   startButton.disabled = true;
-  extraInfoEl.textContent = ' ';
-  countdown()
-  renderBlanks()
+  highscoreButton.disabled = true;
+  extraInfo.textContent = ' ';
+  timer();
+  showQuestion ();  //show question with multiple choice
   
 }
 
 
 
-function countdown() {
+function timer() {
   var timeLeft = 60;
 
   var timeInterval = setInterval(function () {
     if (timeLeft > 1) {
-      timerEl.textContent = timeLeft + ' seconds remaining';
+      countdown.textContent = timeLeft + ' seconds remaining';
       timeLeft--;
     } else if (timeLeft === 1) {
-      timerEl.textContent = timeLeft + ' second remaining';
+      countdown.textContent = timeLeft + ' second remaining';
       timeLeft--;
     } else {
-      timerEl.textContent = '';
+      countdown.textContent = '';
       clearInterval(timeInterval);
-      displayMessage();
+      displayMessage(); //clear the page and show a message: All done! your final score is: correctAnswer, enter your initail and submit
     }
   }, 1000);
 }
+
+
+function showQuestion(){} //write it later
+
+function displayMessage () {
+    questionArea.textContent = ' ',
+    multipleChoiceArea.textContent = ' ';
+    checkAnswer.textContent = "Your final score is: " + correctAnswer
+    resultSubmission.fadein();
+}
+
 
 
 
